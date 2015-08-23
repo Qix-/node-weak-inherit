a1093e3bcc38dfce24d5fa8ed11023f86816ac7c
Qix <i.am.qix@gmail.com>
2015-08-23 15:00:48 -0600

    removed check for .prototype

--- index.js
+++ index.js
@@ -4,10 +4,6 @@ module.exports = function weakInherit(fn) {
 	return function (obj) {
 		fn.apply(obj, [].slice.call(arguments, 1));
 
-		if (!fn.prototype) {
-			return obj;
-		}
-
 		for (var k in fn.prototype) {
 			if (fn.prototype.hasOwnProperty(k)) {
 				obj[k] = fn.prototype[k];
